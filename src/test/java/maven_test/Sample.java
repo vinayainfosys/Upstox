219135c7857b85eda2def9e9163ec29f4d2c5394
vinayainfosys <118234598+vinayainfosys@users.noreply.github.com>
2022-11-15 08:57:43 +0530

    Update Sample.java

--- src/test/java/maven_test/Sample.java
+++ src/test/java/maven_test/Sample.java
@@ -10,7 +10,7 @@ public class Sample {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://termlife.policybazaar.com/prequotes?pb_source=google_brand&pb_medium=ppc&pb_term=Policy%20bazaar&pb_campaign=Policy_Bazaar00Brand&gclid=CjwKCAiA9qKbBhAzEiwAS4yeDbXJbwaDC72p3FZvq_WD6auAWLeyfv4FBqVzTdb5LbswgY1TyuzNixoCoVIQAvD_BwE&pb_content=home_v11");
-		
+		System.out.println("Hi");
 	}
 
 }
